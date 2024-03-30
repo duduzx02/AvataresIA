9ab5c0cd6fa0a2af6747add6f399bb902cdda331
Eduardo Lima <duduzx02@gmail.com>
2024-03-30 04:27:04 -0300

    Criação da tabela AvatarDu para armazenar informações sobre Du Vega.

---
+++ duai/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS avatardu(
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    NAME VARCHAR(255) NOT NULL,
+    LORE TEXT
+);
+
+INSERT INTO avatarDu (name, lore) VALUES
+('Du', 'Conheça Du vega, o prodígio da programação e hacker extraordinário. Desde tenra idade, Du demonstrou
+uma afinidade incrível com computadores e uma capacidade excepcional de entender os sistemas mais complexos.
+Autodidata por natureza, ele dominou diversas linguagens de programação antes mesmo de completar o ensino médio. Sua
+genialidade é equiparada apenas pela sua ética questionável, já que ele navega nos limites da legalidade em sua busca
+ pelo próximo desafio de codificação. Com um conjunto único de habilidades, Du é capaz de invadir sistemas de
+ segurança aparentemente impenetráveis e criar programas revolucionários que desafiam as convenções tecnológicas. Sua
+  personalidade enigmática e seu talento inigualável a tornam uma figura tanto temida quanto admirada na comunidade
+  de hackers clandestinos.')
