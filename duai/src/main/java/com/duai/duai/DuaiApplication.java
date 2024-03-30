c6eb07e8f6198b2068ec66e04c9c760c6a787257
Eduardo Lima <duduzx02@gmail.com>
2024-03-30 02:53:19 -0300

    Habilita o console H2 para o banco de dados H2 in-memory

---
+++ duai/src/main/java/com/duai/duai/DuaiApplication.java
@@ -0,0 +1,13 @@
+package com.duai.duai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuaiApplication.class, args);
+	}
+
+}
---
+++ duai/src/test/java/com/duai/duai/DuaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duai.duai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
